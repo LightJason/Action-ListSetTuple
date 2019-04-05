545d4276f314ba0f243c8e8a4d1619134c2890f8
Philipp Kraus <philipp.kraus@flashpixx.de>
2019-04-06 00:31:08 +0200

    fix style

--- src/main/java/org/lightjason/agentspeak/action/listsettuple/CLambdaStreaming.java
+++ src/main/java/org/lightjason/agentspeak/action/listsettuple/CLambdaStreaming.java
@@ -28,7 +28,6 @@ import org.lightjason.agentspeak.language.execution.lambda.ILambdaStreaming;
 
 import javax.annotation.Nonnull;
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Stream;
 
 
